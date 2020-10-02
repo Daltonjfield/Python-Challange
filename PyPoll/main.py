eca499b3e64a734a8435f8af957d6f18ab72481c
Dalton Field <daltonfield@Daltons-MacBook-Pro.local>
2020-10-02 17:28:23 -0400

    Created a list with candidate names and vote counts in ascending order.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,5 +1,13 @@
 import os
 import csv
+import collections
+from collections import Counter
+
+#Variables
+candidates = []
+total_candidate_votes = []
+
+
 csvpath = os.path.join('Resources', 'election_data.csv')
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -7,5 +15,25 @@ with open(csvpath, newline='') as csvfile:
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
     for row in csvreader:
-        print(row)
+        #print(row)
+        candidates.append(row[2])
+
+    voter_list = sorted(candidates)
+    #print(f'{voter_list}')
+
+    candidates_list = Counter(voter_list)
+    #print(f'{candidates}')
+
+    total_candidate_votes.append(candidates_list.most_common())
+    print(f'{total_candidate_votes}')
+    
+
+
+    
+
+
+
+
+
         
+
